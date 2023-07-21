b141d3f50d3cc76e4a5cd4b58a241bdea08b1b72
LifaMuzila <140125177+LifaMuzila@users.noreply.github.com>
2023-07-21 02:04:11 +0200

    Add files via upload

---
+++ BasicFramePracticeApp.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package basicframepracticeapp;
+
+import za.ac.tut.ui.FramePractice1;
+
+/**
+ *
+ * @author Lifa Muzila
+ */
+public class BasicFramePracticeApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      FramePractice1 frame;
+      frame = new FramePractice1();
+        System.out.println("This is my first frame practice");
+    }
+    
+}
---
+++ FramePractice1.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package za.ac.tut.ui;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Lifa Muzila
+ */
+public class FramePractice1 extends JFrame{
+    public FramePractice1(){
+        setTitle("FIRST FRAME PRACTICE");
+        setSize(200,250);
+        setDefaultLookAndFeelDecorated(true);
+        setResizable(true);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+    
+}
